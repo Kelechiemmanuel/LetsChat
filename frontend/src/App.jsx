58af70640f70cc07f93b3671e9ddbbcff98ac2ae
Kelechi <ekelechi413@gmail.com>
2026-04-27 12:51:01 +0100

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import Login from "./component/Login";
 import Chat from "./component/Chat";
 import Register from "./component/Register";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter, HashRouter } from "react-router-dom";
 
 const App = () => {
   const token = localStorage.getItem("token");
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={token ? <Navigate to="/chat" /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chat" element={token ? <Chat /> : <Navigate to="/login" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+
   );
 };
 
